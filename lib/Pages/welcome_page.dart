b89e7271481e23187de9eee7d39e9581ca7625db
WillDoIt2022 <uteamproject2022@gmail.com>
2022-12-27 15:15:23 +0200

    Log in page added

--- lib/Pages/welcome_page.dart
+++ lib/Pages/welcome_page.dart
@@ -14,12 +14,14 @@ class WelcomePage extends StatefulWidget {
 
 class _LaunchApp extends State<WelcomePage> {
   dynamic _timer;
+  dynamic _next;
   @override
   void initState() {
     super.initState();
+    _next = true;
     _timer = true;
     //Go to Log in password page in a 5 seconds
-    Timer(Duration(seconds: 3), () {
+    Timer(Duration(seconds: 5), () {
       setState(
         () {
           _timer = false;
@@ -32,102 +34,157 @@ class _LaunchApp extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false, //remove the debug banner "Demo"
-      title: 'UTEAM',
+      title: _timer?'UTEAM WELCOME':"Log in password",
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 56, 58, 63),
-        body:  Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // ignore: prefer_const_literals_to_create_immutables
-                children: <Widget>[
-                  Stack(
-                    children: [
-                      SizedBox(
-                        width: 390,
-                        height: 400,
-                      ),
-                      Positioned(
-                        right: 18,
-                        top: 0,
-                        child: Container(
-                          width: 390,
-                          height: 400,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('img/Logo_guy.png'),
-                              fit: BoxFit.contain,
-                            ),
+          backgroundColor: Color.fromARGB(255, 56, 58, 63),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              !_timer
+                  ? _next?Align(
+                      alignment: Alignment.topRight,
+                      child: SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                           elevation: 0.0,
+                            backgroundColor: Colors.black.withOpacity(0.05),
+                            textStyle: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,),
+                            // background
                           ),
+                          onPressed: () {
+                             setState(
+                  () {
+                    if(_next){_next = !_next;}
+                    
+                  },
+                );
+                          },
+                          child: Text('next>'.toUpperCase()),
                         ),
                       ),
-                      Positioned(
-                        right: 30,
-                        top: 45,
-                        child: Container(
-                          width: 139,
-                          height: 140,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('img/Logo_house.png'),
-                              fit: BoxFit.contain,
-                            ),
+                    )
+                  :Align(
+                      alignment: Alignment.topLeft,
+                      child: SizedBox(
+                        width: 150,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                           elevation: 0.0,
+                            backgroundColor: Colors.black.withOpacity(0.05),
+                            textStyle: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,),
+                            // background
                           ),
+                          onPressed: () {
+                             setState(
+                  () {
+                    if(!_next){_next = !_next;}
+                  },
+                );
+                          },
+                          child: Text('<back'.toUpperCase()),
                         ),
                       ),
-                      Positioned(
-                        right: 30,
-                        top: 170,
-                        child: Container(
-                          width: 37,
-                          height: 37,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('img/Logo_dashbord.png'),
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        right: 55,
-                        top: 190,
-                        child: Container(
-                          width: 85,
-                          height: 85,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('img/Logo_box.png'),
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ): Container(),
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 390,
+                    height: 400,
                   ),
-                  _timer?
-                  Column(
-                    children: <Widget>[
-                  Text(
-                    "WELCOME TO",
-                    style: TextStyle(
-                      fontSize: 36,
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w700,
+                  Positioned(
+                    right: 18,
+                    top: 0,
+                    child: Container(
+                      width: 390,
+                      height: 400,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('img/Logo_guy.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
                     ),
                   ),
-                  Text(
-                    "UTEAM",
-                    style: TextStyle(
-                      fontSize: 36,
-                      color: Color.fromARGB(255, 124, 160, 209),
-                      fontFamily: 'Inter',
-                      fontWeight: FontWeight.w700,
+                  Positioned(
+                    right: 30,
+                    top: 45,
+                    child: Container(
+                      width: 139,
+                      height: 140,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('img/Logo_house.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
                     ),
-                ),],): LoginWidget(),
+                  ),
+                  Positioned(
+                    right: 30,
+                    top: 170,
+                    child: Container(
+                      width: 37,
+                      height: 37,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('img/Logo_dashbord.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 55,
+                    top: 190,
+                    child: Container(
+                      width: 85,
+                      height: 85,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('img/Logo_box.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
-              )
-            
-      ),
+              ),
+              _timer
+                  ? Column(
+                      children:  [
+                        Text(
+                          "welcome to".toUpperCase(),
+                          style: TextStyle(
+                            fontSize: 36,
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Text(
+                          "UTEAM",
+                          style: TextStyle(
+                            fontSize: 36,
+                            color: Color.fromARGB(255, 124, 160, 209),
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ],
+                    )
+                  : loginWidget(_next),
+            ],
+          )),
     );
   }
 }
--- lib/Widgets/add_phone.dart
+++ lib/Widgets/add_phone.dart
@@ -1,39 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:easy_mask/easy_mask.dart';
+import 'dart:math';
 // ignore_for_file: prefer_const_constructors
 
-Widget LoginWidget() {
-  return Column(children: <Widget>[
-    Text(
-      textAlign: TextAlign.center,
-      "ENTER YOUR\nPHONE NUMBER",
-      style: TextStyle(
-        fontSize: 36,
-        color: Color.fromARGB(255, 255, 255, 255),
-        fontFamily: 'Inter',
-        fontWeight: FontWeight.w700,
-      ),
-    ),
-    SizedBox(
-      width: 290,
-    child:TextField(
-      style: TextStyle(
-        fontSize: 16,
-        color: Color.fromARGB(255, 255, 255, 255),
-        fontFamily: 'Inter',
-        fontWeight: FontWeight.w700,
-      ),
-      keyboardType: TextInputType.number,
-      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      maxLines: 1,
-      decoration: InputDecoration(
-        enabledBorder: UnderlineInputBorder(      
-                      borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255)),   
-                      ),  
-              focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
-                   ),  
-          border: InputBorder.none, labelText: '', hintText: ''),
-    ),),
-  ]);
+Widget loginWidget(_next) {
+  Random random = Random();
+  int randomNumber =
+      random.nextInt(9999) + 1000; // from 1000 upto 9999 included
+  print(randomNumber);
+  print(_next);
+
+  return 
+ 
+  _next
+      ? Column(children: <Widget>[  
+          SizedBox(
+            height:125,
+            child: Text(
+            textAlign: TextAlign.center,
+            "enter your\nphone number".toUpperCase(),
+            style: TextStyle(
+              fontSize: 36,
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w700,
+            ),
+          ),),
+          SizedBox(
+            width: 290,
+            child: TextField(
+              style: TextStyle(
+                fontSize: 16,
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
+              ),
+              keyboardType: TextInputType.number,
+              controller: TextEditingController(text: "+33"),
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                TextInputMask(mask: '\\+99 999 999 99 99', reverse: false)
+              ],
+              maxLines: 1,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+                border: InputBorder.none,
+                labelText: '',
+                hintText: '',
+                contentPadding: EdgeInsets.only(left: 24.0),
+              ),
+            ),
+          ),
+        ])
+      : Column(
+          children: <Widget>[
+            SizedBox(
+            height:125,
+            child:Text(
+              textAlign: TextAlign.center,
+              "enter code we\nsended on your\nphone number".toUpperCase(),
+              style: TextStyle(
+                fontSize: 36,
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w700,
+              ),
+            ),),
+            SizedBox(
+              width: 290,
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w700,
+                ),
+                keyboardType: TextInputType.number,
+                controller: TextEditingController(text: ""),
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(4),
+                ],
+                maxLines: 1,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                  border: InputBorder.none,
+                  labelText: '',
+                  hintText: '',
+                  contentPadding: EdgeInsets.only(left: 24.0),
+                ),
+              ),
+            ),
+          ],
+        );
 }
